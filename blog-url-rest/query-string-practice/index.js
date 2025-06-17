2480a70912e4ffe4b5642c2f8e981677da9abf40
Richard105247908 <105247908@aston.ac.uk>
2025-06-17 12:10:51 +0100

    query string practice

---
+++ blog-url-rest/query-string-practice/index.js
@@ -0,0 +1,17 @@
+/**
+ * Challenge part 1: GET the current weather for your city with 
+ * the Open Weather API and log it to the console.
+ * 
+ * BaseURL: https://apis.scrimba.com/openweathermap/data/2.5/
+ * Endpoint: /weather
+ * Query: ??? (https://openweathermap.org/current)
+    * NOTE: It says you need to include `appid` in your query, but you can skip that this time
+    
+    
+Challenge part 2: change the units into something that makes more sense to you
+than Kelvin 😂
+ */
+
+fetch("https://apis.scrimba.com/openweathermap/data/2.5/weather?q=London&units=metric")
+    .then(res => res.json())
+    .then(data => console.log(data))
