96e24cf36826372aac0884a630f0411c3504a6c6
Richard105247908 <105247908@aston.ac.uk>
2025-06-23 13:17:37 +0100

    challenge 1 completed

---
+++ Async-callbacks-war/method-chaining/index.js
@@ -0,0 +1,13 @@
+/**
+ * Challenge: method chaining!
+ * 
+ * 1. Select the button in the DOM and add an event listener to it without saving the DOM element as a separate variable. I.e. "chain" the `addEventListener` on after your `getElementById()`(When clicked, log "Clicked" to the console)
+ *    - I realize this might feel like busywork, but my intent will make sense soon
+ * 
+ * 2. Given the array below, chain the `.filter` and `.map` array methods together to turn the array into an array of string email addresses of only the people in the array who voted. Log the array of email addresses to the console
+ */
+
+
+// document.getElementById("new-deck").addEventListener("click", function() {
+//     console.log("Clicked!")
+// })
