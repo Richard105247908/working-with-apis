95a0a0c521d135e020e0540c02267c0713668a1d
Richard105247908 <105247908@aston.ac.uk>
2025-06-23 10:18:48 +0100

    challenge

---
+++ Async-callbacks-war/callbacks-make-filterArray-funct/index.js
@@ -0,0 +1,34 @@
+/**
+ * Challenge: 
+ * 
+ * Write your own `filter` function! Don't worry about adding it to the prototype of arrays or anything.
+ * This function should take 2 parameters:
+ * 1. The array you want to filter through, and
+ * 2. A callback function
+ * 
+ * Steps for filterArray function logic:
+ * 1. Initialize a new, empty array which will be returned at the end of the `filterArray`s operations (Completed ✅)
+ * 2. Loop through the array passed as the 1st parameter
+ * 3. Inside the loop, call the callback function, passing the individual item you're currently looping over as the argument to your callback function
+ * 4. If the callback function returns `true`, push the current item you're iterating on in the loop to the new array. If it returns `false`, don't push it to the array.
+ * 5. When the loop is over, return the new array
+ */
+
+const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+]
+
+function filterArray(array, callback) {
+    const resultingArray = []
+    for(let i=0; i< array.length-1;i++){
+        if(callback(array[i]===true)){
+            push(array[i]).resultingArray
+        }
+        
+    }
+    // Write your filtering logic here
+    return resultingArray;
+}
