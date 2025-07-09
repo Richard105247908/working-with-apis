86c6c7cdab2af1894699ba02131003639726f543
Richard105247908 <105247908@aston.ac.uk>
2025-07-09 14:22:39 +0100

    displayed remain cards

--- Async-callbacks-war/war/index.js
+++ Async-callbacks-war/war/index.js
@@ -3,6 +3,7 @@ const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 const winnerElement=document.getElementById("winner")
+const remainingText = document.getElementById("remaining") 
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -18,7 +19,8 @@ drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            //console.log(data)
+            remainingText.textContent= `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
