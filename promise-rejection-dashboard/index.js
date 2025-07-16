8e54cff098a53cbbedbf419ba4633133f69dac8d
Richard105247908 <105247908@aston.ac.uk>
2025-07-16 12:53:47 +0100

    catch error added to crypto coin

--- promise-rejection-dashboard/index.js
+++ promise-rejection-dashboard/index.js
@@ -27,6 +27,10 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(data =>{
         console.log(data)
     })
+	.catch(err=>{
+        console.log(err)
+        
+    })
     
 /**
  * {
