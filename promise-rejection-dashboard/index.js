4e636bcdf1dfb8effe82702aa665f265faeba4a3
Richard105247908 <105247908@aston.ac.uk>
2025-07-16 12:39:51 +0100

    catch method added

--- promise-rejection-dashboard/index.js
+++ promise-rejection-dashboard/index.js
@@ -15,6 +15,18 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		paraTag.innerText=`By: ${data.user.name}`;
     })
+	.catch(err=>{
+		 document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
+
+	})
+
+	fetch("https://api.coingecko.com/api/v3/coins/dogecoin?localization=true")
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+    })
     
 /**
  * {
